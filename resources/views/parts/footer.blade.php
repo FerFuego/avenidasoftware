cfd2bf04423d3850dde6fdd77ace1b61b3d0a4a8
Fer Catalano <fernando@hazak.tech>
2024-09-02 22:58:48 -0300

    Fix version

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> v{{ env('APP_VERSION', '1.0') }}
     </div>
-    <strong>Copyright &copy; 2020 {{ config('app.name', 'Laravel') }}.</strong> Todos los derechos reservados.
+    <strong>Copyright &copy; {{ date('Y') }} {{ config('app.name', 'Laravel') }}.</strong> Todos los derechos reservados.
 </footer>
